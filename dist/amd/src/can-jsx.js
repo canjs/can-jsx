42b5e7e028d79e3d4a700a5ff2d9e68b89650c23
Christopher Baker <christopher@bitovi.com>
2018-02-15 16:32:46 -0800

    0.0.1

---
+++ dist/amd/src/can-jsx.js
@@ -0,0 +1,8 @@
+/*can-jsx@0.0.0#src/can-jsx*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = function () {
+        return 'This is the can-jsx plugin';
+    };
+});
